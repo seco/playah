9cf7ff774dc5e6b6b57725607e5dada30181b613
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-12-21 09:16:27 +0100

    test: tweak per latest amends

--- test.js
+++ test.js
@@ -2,10 +2,10 @@ import 'cutaway'
 import { report, assert } from 'tapeless'
 import createPlayer from './index.es'
 
-const { notOk } = assert
+const { ok, notOk } = assert
 const { status } = createPlayer()
 
-notOk(status.busy, 'running state', 'will report')
+ok(status.idle, 'running state', 'will report')
 notOk(status.time, 'current time')
 
 report()
